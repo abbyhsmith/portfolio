f790d66fc4d66023ec3af15b03db3766e6405342
Abby <smitha9294@gmail.com>
2021-06-15 15:04:47 -0500

    added projects, fixed nav

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import animalFactsPic from '../images/animalfacts.png'
+import wowEnemyPic from '../images/wow.png'
+import groceryListPic from '../images/grocerylist.png'
 
 const Projects = () => {
 	return (
 		<div id="projects">
 			<h1>My Projects</h1>
+			<div className="projectContainer">
+				<div className="projectDiv">
+					<a href="/animalfacts">
+						<img src={animalFactsPic} />
+						<div className="projectTitle">Animal Facts</div>
+					</a>
+				</div>
+				<div className="projectDiv">
+					<a href="/wow-enemy-finder">
+						<img src={wowEnemyPic} />
+						<div className="projectTitle">World of Warcraft Enemy Search</div>
+					</a>
+				</div>
+				<div className="projectDiv">
+					<a href="/grocery-list-maker">
+						<img src={groceryListPic} />
+						<div className="projectTitle">Grocery List Maker</div>
+					</a>
+				</div>
+			</div>
 		</div>
 	)
 }
